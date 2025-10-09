c52da65dc79c74ee80b634295f0a9dc8cf197670
Torres116 <joaotorres0022@gmail.com>
2025-10-09 03:08:25 +0100

    refactored naming convention handler

--- Formatter/Formatter/handlers/NamingConventionHandler.cs
+++ Formatter/Formatter/handlers/NamingConventionHandler.cs
@@ -4,24 +4,50 @@ public static class NamingConventionHandler
 {
     public static string ToCamelCase(string str)
     {
-        str = str.Replace("_", "");
-        var strArray = str.ToCharArray();
-        strArray[0] = char.ToLower(strArray[0]);
-        return new string(strArray);
+        string temp = str[0].ToString().ToLower();
+        for (var i = 1; i < str.Length; i++)
+        {
+            if (str[i] == '_')
+            {
+                while (i < str.Length && str[i] == '_')
+                    i++;
+                
+                temp += char.ToUpper(str[i]);
+                continue;
+            }
+            
+            temp += str[i];
+        }
+
+        return temp;
     }
 
     public static string ToPascalCase(string str)
     {
-        str = str.Replace("_", "");
-        var strArray = str.ToCharArray();
-        strArray[0] = char.ToUpper(strArray[0]);
-        return new string(strArray);
+        string temp = str[0].ToString().ToUpper();
+        for (var i = 1; i < str.Length; i++)
+        {
+            if (str[i] == '_')
+            {
+                while (i < str.Length && str[i] == '_')
+                    i++;
+                
+                temp += char.ToUpper(str[i]);
+                continue;
+            }
+            
+            temp += str[i];
+        }
+
+        return temp;
     }
 
     public static string ToSnakeCase(string str)
     {
         string result = "";
-        str = str.Replace("_", "");
+        if(str.Contains("_"))
+            str = ToPascalCase(str);
+        
         for (var i = 0; i < str.Length; i++)
         {
             var c = str[i];
